0d80f54a073868525580eece18a0c5fe4200122e
Davi Leitao <davi.leitao@hotmail.com>
2024-10-31 11:08:28 -0300

    add nvim ts autotag to custom plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -23,4 +23,17 @@ return {
       { '<leader>gl', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
     },
   },
+  {
+    'windwp/nvim-ts-autotag',
+    ft = {
+      'javascript',
+      'javascriptreact',
+      'typescript',
+      'typescriptreact',
+      'html',
+    },
+    config = function()
+      require('nvim-ts-autotag').setup()
+    end,
+  },
 }
